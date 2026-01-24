c8b17786539d144c0f1b09302bc5f1f39c48d715
Antonio Rodriguez <ajcomputerses@gmail.com>
2026-01-24 10:33:05 +0100

    cleanup: Remove temporary backup files from header unification

--- convert_headers.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for file in "$@"; do
-    echo "Processing $file"
-    awk '
-    NR < 10 {print; next}
-    /^---$/ {print "##"; next}
-    {print}
-    ' "$file" > "${file}.tmp" && mv "${file}.tmp" "$file"
-done
